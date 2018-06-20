fa5afd1c225f248c9227228e4b6704f2eb45cc68
zhangyu163 <hebzhangyu@163.com>
2018-06-20 05:29:39 -0700

    connect factory and strategy

--- Strategy/main.cc
+++ Strategy/main.cc
@@ -13,27 +13,9 @@ using std::pair;
 
 int main()
 {
-	CashContext* pContext = nullptr;
-	map<string, int> typemap;
-	typemap.insert(pair<string, int>("normal", 0));
-	typemap.insert(pair<string, int>("dis8", 1));
-	typemap.insert(pair<string, int>("dis7", 2));
-	typemap.insert(pair<string, int>("ret1", 3));
 	string cash_type;
 	cin >> cash_type;
-	switch(typemap[cash_type])
-	{
-		case 0: pContext = new CashContext(new NormalCash());
-				break;
-		case 1: pContext = new CashContext(new DiscountCash(0.8));
-				break;
-		case 2: pContext = new CashContext(new DiscountCash(0.7));
-				break;
-		case 3: pContext = new CashContext(new ReturnCash(300, 100));
-				break;
-		default: pContext = new CashContext(new NormalCash());
-				break;
-	}
+	CashContext* pContext = new CashContext(cash_type);
 	double total = 700;
 	double result = 0;
 	result = pContext->GetResult(total);
--- Strategy/strategyclass.h
+++ Strategy/strategyclass.h
@@ -1,15 +1,37 @@
 #ifndef _STRATEGY_H_
 #define _STRATEGY_H_ 
 
+#include<string>
+#include<map>
 #include"classCollection.h"
+
+using std::string;
+using std::map;
+using std::pair;
 
 class CashContext
 {
 	public:
-		CashContext(CashSuper* pcash)
-			: m_pCash(pcash)
+		CashContext(string type)
 		{
-
+			map<string, int> typemap;
+			typemap.insert(pair<string, int>("normal", 0));
+			typemap.insert(pair<string, int>("dis8", 1));
+			typemap.insert(pair<string, int>("dis7", 2));
+			typemap.insert(pair<string, int>("ret1", 3));
+			switch(typemap[type])
+			{
+				case 0: m_pCash = new NormalCash();
+						break;
+				case 1: m_pCash = new DiscountCash(0.8);
+						break;
+				case 2: m_pCash = new DiscountCash(0.7);
+						break;
+				case 3: m_pCash = new ReturnCash(300, 100);
+						break;
+				default: m_pCash = new NormalCash();
+						break;
+			}
 		}
 		
 		double GetResult(double money)
@@ -17,7 +39,7 @@ class CashContext
 			return m_pCash->AcceptCash(money);
 		}
 	private:
-		CashSuper* m_pCash;
+		CashSuper* m_pCash = nullptr;
 };
 
 #endif
